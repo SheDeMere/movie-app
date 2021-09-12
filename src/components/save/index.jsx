adb14f1e822cc1cac31df541e330b479a0de9d20
Abdulaev Apti <apti2500@mail.ru>
2021-09-12 17:08:17 +0300

    added remove button

--- src/components/save/index.jsx
+++ src/components/save/index.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import styles from './Save.module.scss'
 import Save from "./Save";
+import {ImBin2} from 'react-icons/im'
 
 function Index() {
     const movies = JSON.parse(localStorage.getItem('savedMovies'))
+    console.log(movies)
+    const handleRemove = (e) => {
+            window.location.reload()
+            localStorage.removeItem('savedMovies')
+    }
     return (
         <div className={styles.saveMain}>
             {movies === null ?
             <div className={styles.empty}>
-                <h1>Пожалуйста, сохраните что-нибудь :(</h1>
+                <h1>Cохрани что-нибудь :(</h1>
                 <img src="https://media3.giphy.com/media/7SF5scGB2AFrgsXP63/giphy.gif?cid=ecf05e47v2avk7coqka1h3tc4m8za78p2iqstm7q8h4blxkr&rid=giphy.gif&ct=g" alt=""/>
             </div>
                 :
-                movies.map((item, index) => {
-                    return <Save  movie={item} key={index}/>
-                })
+                <div className={styles.saveMainItems}>
+                    <button  onClick={handleRemove}>
+                        <ImBin2 />
+                    </button>
+                    {
+                        movies.map((item, index) => {
+                            return <Save  movie={item} key={index}/>
+                        })
+                    }
+                </div>
             }
         </div>
     );
